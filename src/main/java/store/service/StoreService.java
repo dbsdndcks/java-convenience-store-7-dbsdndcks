700817f02b6af452c963f8fc3233160098d5dd29
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-09 07:56:27 +0900

    feat: 상품 결제 기능 일부 구현

--- src/main/java/store/service/StoreService.java
+++ src/main/java/store/service/StoreService.java
@@ -2,9 +2,11 @@ package store.service;
 
 import store.domain.Product;
 import store.domain.Products;
+import store.domain.Promotion;
 import store.domain.Promotions;
 import store.util.parser.InputParser;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
@@ -22,16 +24,26 @@ public class StoreService {
     }
 
     public void processPayment(String input) {
-        //InputValidator.validatePurchaseInput(input);
         Map<String, Integer> parsedAnswer = InputParser.purchaseParse(input);
-        for (Map.Entry<String, Integer> answer : parsedAnswer.entrySet()) {
-            String requestName = answer.getKey();
-            int requestQuantity = answer.getValue();
+        LocalDate today = LocalDate.now();
 
-            List<Product> requestProduct = products.findByName(requestName);
-            if (requestProduct.isEmpty()) {
-                throw new IllegalArgumentException("존재하지 않는 상품");
-            }
+        for (Map.Entry<String, Integer> answer : parsedAnswer.entrySet()) {
+            handleProductPurchase(answer.getKey(), answer.getValue(), today);
         }
     }
+
+    private void handleProductPurchase(String productName, int quantity, LocalDate date) {
+        List<Product> productsList = products.findByName(productName);
+        if (productsList.isEmpty()) throw new IllegalArgumentException("존재하지 않는 상품: " + productName);
+        int remainingQuantity = quantity;
+        int finalPrice = 0;
+        for (Product product : productsList) {
+            if (remainingQuantity <= 0) break;
+            finalPrice += processProductPurchase(product, remainingQuantity);
+            remainingQuantity = updateRemainingQuantity(product, remainingQuantity);
+        }
+    }
+
+   
+
 }
