0a65af60cbb580bfb8ac55d7f3ef3e5cc93a6bab
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-08 19:37:36 +0900

    feat: 컨트롤러 설계 추가

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -1,4 +1,38 @@
 package store.controller;
 
+import store.domain.Products;
+import store.service.FileDataLoaderService;
+import store.service.StoreService;
+import store.util.parser.InputParser;
+import store.util.validator.InputValidator;
+import store.view.InputView;
+import store.view.OutputView;
+
+import java.util.Map;
+
 public class StoreController {
+    private InputView inputView;
+    private OutputView outputView;
+    private StoreService storeService;
+
+    public StoreController(InputView inputView, OutputView outputView, StoreService storeService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.storeService = storeService;
+    }
+
+    public void run() {
+        storeOpeningMessage();
+        customerPurchase();
+    }
+
+    private void storeOpeningMessage() {
+        String openiningMent = storeService.generateOpeningMessage();
+        outputView.printOpeningMent(openiningMent);
+    }
+
+    private void customerPurchase() {
+        String answer = inputView.getPurchaseinfo();
+        storeService.processPayment(answer);
+    }
 }
