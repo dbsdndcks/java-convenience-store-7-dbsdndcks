aa3cc27fd63ff9ce103070a396975d1ddd989ad9
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-08 19:34:30 +0900

    feat: AppConfig 설계

--- src/main/java/store/config/AppConfig.java
+++ src/main/java/store/config/AppConfig.java
@@ -1,4 +1,34 @@
 package store.config;
 
+import store.controller.StoreController;
+import store.service.FileDataLoaderService;
+import store.service.StoreService;
+import store.util.File.FileReader;
+import store.view.InputView;
+import store.view.OutputView;
+
 public class AppConfig {
+    public StoreController storeController() {
+        return new StoreController(inputView(), outputView(), storeService());
+    }
+
+    private InputView inputView() {
+        return new InputView();
+    }
+
+    private OutputView outputView() {
+        return new OutputView();
+    }
+
+    private StoreService storeService(){return fileDataLoader().initializeStoreService();}
+
+    public FileDataLoaderService fileDataLoader() {
+        return new FileDataLoaderService(fileReader());
+    }
+
+    private FileReader fileReader() {
+        return new FileReader();
+    }
+
+
 }
