cc209855428ab5260520614d754efbb4055cd9a0
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-08 19:38:39 +0900

    feat: 상품 상세 정보를 저장하는 Product 도메인 클래스 추가

--- src/main/java/store/domain/Product.java
+++ src/main/java/store/domain/Product.java
@@ -1,4 +1,49 @@
 package store.domain;
 
+import java.text.DecimalFormat;
+
 public class Product {
+    private static final DecimalFormat priceFormat = new DecimalFormat("#,###");
+    private static final String KOREA_WON = "원 ";
+    private static final String STOCK_COUNT = "개 ";
+    private static final String OUT_OF_STOCK = "재고 없음 ";
+
+    private String name;
+    private int price;
+    private int quantity;
+    private String promotion;
+
+
+    public Product(String name, int price, int quantity, String promotion) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.promotion = promotion;
+    }
+
+    public String formatProductInfo() {
+        StringBuilder productInfo = new StringBuilder();
+
+        productInfo.append(name).append(" ")
+                .append(priceFormat.format(price)).append(KOREA_WON)
+                .append(formatQuantity(quantity))
+                .append(promotion);
+        return productInfo.toString();
+    }
+
+    private String formatQuantity(int quantity) {
+        String stock = quantity + STOCK_COUNT;
+        if (quantity < 0) {
+            return OUT_OF_STOCK;
+        }
+        return stock;
+    }
+
+    public boolean productNameEqual(String productName) {
+        return this.name.equals(productName);
+    }
+
+
+
+
 }
