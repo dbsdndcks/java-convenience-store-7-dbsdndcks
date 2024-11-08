b41ebb389306e42ca9a7dc845f929769b1e08f2b
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-08 19:38:58 +0900

    feat: 프로모션 상세 정보를 저장하는 Promotion도메인 클래스 추가

--- src/main/java/store/domain/Promotion.java
+++ src/main/java/store/domain/Promotion.java
@@ -1,4 +1,30 @@
 package store.domain;
 
+import java.time.LocalDate;
+
+import camp.nextstep.edu.missionutils.DateTimes;
+
 public class Promotion {
+    private String name;
+    private int buy;
+    private int get;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public Promotion(String name, int buy, int get, LocalDate startDate, LocalDate endDate) {
+        this.name = name;
+        this.buy = buy;
+        this.get = get;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public boolean isActive() {
+        LocalDate today = DateTimes.now().toLocalDate();
+        return (today.equals(startDate) || today.isAfter(startDate)) && (today.equals(endDate) || today.isBefore(endDate));
+    }
+
+    public boolean promotionNameEqual(String promotionName) {
+        return (this.name.equals(promotionName));
+    }
 }
