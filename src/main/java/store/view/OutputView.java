88880a9a76e2bbde47de0efe7e961c4ebaeae327
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-10 16:05:35 +0900

    feat: 영수증 출력 기능 추가

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,7 +1,14 @@
 package store.view;
 
+import store.domain.Product;
+import store.domain.Receipt;
+
 public class OutputView {
     public void printOpeningMent(String openiningMent) {
         System.out.println(openiningMent);
+    }
+
+    public void printReceipt(String output) {
+        System.out.println(output);
     }
 }
