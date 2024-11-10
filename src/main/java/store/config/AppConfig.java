ee1ba6a73df1a438c832c861568e1e43b81d3aa7
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-10 21:17:32 +0900

    feat: 서비스에서 입력을 위한 인터페이스 기능 도입

--- src/main/java/store/config/AppConfig.java
+++ src/main/java/store/config/AppConfig.java
@@ -1,15 +1,27 @@
 package store.config;
 
 import store.controller.StoreController;
+import store.domain.Products;
+import store.domain.Promotions;
 import store.service.FileDataLoaderService;
 import store.service.StoreService;
+import store.service.UserInteractionCallback;
 import store.util.File.FileReader;
 import store.view.InputView;
 import store.view.OutputView;
 
 public class AppConfig {
     public StoreController storeController() {
-        return new StoreController(inputView(), outputView(), storeService());
+        StoreController controller = new StoreController(inputView(), outputView());
+        StoreService service = storeService(controller);
+        controller.setStoreService(service);
+        return controller;
+    }
+
+    private StoreService storeService(UserInteractionCallback callback) {
+        Products products = fileDataLoader().loadProductsFromFile();
+        Promotions promotions = fileDataLoader().loadPromotionsFromFile();
+        return new StoreService(products, promotions, callback);
     }
 
     private InputView inputView() {
@@ -20,8 +32,6 @@ public class AppConfig {
         return new OutputView();
     }
 
-    private StoreService storeService(){return fileDataLoader().initializeStoreService();}
-
     public FileDataLoaderService fileDataLoader() {
         return new FileDataLoaderService(fileReader());
     }
@@ -29,6 +39,5 @@ public class AppConfig {
     private FileReader fileReader() {
         return new FileReader();
     }
-
-
 }
+
--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -3,39 +3,54 @@ package store.controller;
 import store.domain.Receipt;
 import store.service.StoreService;
 
+import store.service.UserInteractionCallback;
+import store.util.exception.RestartException;
 import store.view.InputView;
 import store.view.OutputView;
 
 
-public class StoreController {
+public class StoreController implements UserInteractionCallback {
     private InputView inputView;
     private OutputView outputView;
     private StoreService storeService;
 
-    public StoreController(InputView inputView, OutputView outputView, StoreService storeService) {
+    public StoreController(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
+    }
+
+    public void setStoreService(StoreService storeService) {
         this.storeService = storeService;
     }
 
     public void run() {
-        storeOpeningMessage();
-        customerPurchase();
-        membershipMessage();
+        try {
+            storeOpeningMessage();
+            customerPurchase();
+            membershipMessage();
+        } catch (RestartException re) {
+            run();
+        }
     }
-
     private void storeOpeningMessage() {
         String openiningMent = storeService.generateOpeningMessage();
-        outputView.printOpeningMent(openiningMent);
+        outputView.printProducts(openiningMent);
     }
 
     private void customerPurchase() {
-        String answer = inputView.getPurchaseinfo();
+        String answer = inputView.readItem();
         Receipt receipt = storeService.processPayment(answer);
         outputView.printReceipt(receipt.generateReceiptString());
     }
 
 
     private void membershipMessage() {
+    }
+
+
+    @Override
+    public boolean askUser(String message) {
+        String answer = inputView.InputMessage(message);
+        return "Y".equals(answer);
     }
 }
