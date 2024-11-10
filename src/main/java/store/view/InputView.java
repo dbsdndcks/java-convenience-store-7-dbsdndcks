b6fd813d8d6a75f3495615b81d9c9f240d986c57
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-10 21:16:30 +0900

    refactor: InputView,OutputView 메서드명 수정

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -3,8 +3,15 @@ package store.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public String getPurchaseinfo() {
+    public String readItem() {
         System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
-        return Console.readLine();
+        String input = Console.readLine();
+        return input;
+    }
+
+    public String InputMessage(String message) {
+        System.out.println(message);
+        String input = Console.readLine();
+        return input;
     }
 }
--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,14 +1,12 @@
 package store.view;
 
-import store.domain.Product;
-import store.domain.Receipt;
-
 public class OutputView {
-    public void printOpeningMent(String openiningMent) {
+    public void printProducts(String openiningMent) {
         System.out.println(openiningMent);
     }
 
     public void printReceipt(String output) {
         System.out.println(output);
     }
+
 }
