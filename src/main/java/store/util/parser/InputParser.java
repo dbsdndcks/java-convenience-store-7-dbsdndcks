5d5eeda585532aac832927e3e7414e2b459253d5
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-11 10:59:44 +0900

    refactor: InputParser 예외 추가

--- src/main/java/store/util/parser/InputParser.java
+++ src/main/java/store/util/parser/InputParser.java
@@ -10,6 +10,9 @@ public class InputParser {
         String[] items = input.replace("[", "").replace("]", "").split(",");
         for (String item : items) {
             String[]parts = item.split("-");
+            if (parts.length != 2) {
+                throw new IllegalArgumentException("[ERROR] 입력 형식이 올바르지 않습니다. 예: [사이다-2],[감자칩-1]");
+            }
             String productName = parts[0];
             int quantity = Integer.parseInt(parts[1]);
             purchasedProducts.put(productName, quantity);
