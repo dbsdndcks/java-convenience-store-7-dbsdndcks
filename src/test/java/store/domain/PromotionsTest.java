3032c641ed102d097abaccdceed7415426c894fa
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-11 22:21:05 +0900

    test: Promotions 도메인 테스트 추가

--- src/test/java/store/domain/PromotionsTest.java
+++ src/test/java/store/domain/PromotionsTest.java
@@ -1,4 +1,28 @@
 package store.domain;
 
-public class PromotionsTest {
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PromotionsTest {
+
+    @Test
+    void 프로모션_찾기_이름으로() {
+        Promotion promotion = new Promotion("탄산2+1", 2, 1, LocalDate.now(), LocalDate.now().plusDays(10));
+        Promotions promotions = new Promotions(List.of(promotion));
+
+        Promotion foundPromotion = promotions.promotionFindByName("탄산2+1");
+        assertThat(foundPromotion).isNotNull();
+        assertThat(foundPromotion.promotionNameEqual("탄산2+1")).isTrue();
+    }
+
+    @Test
+    void 프로모션_존재하지_않을때_확인() {
+        Promotions promotions = new Promotions(List.of(new Promotion("탄산2+1", 2, 1, LocalDate.now(), LocalDate.now().plusDays(10))));
+        Promotion result = promotions.promotionFindByName("탄산3+1");
+        assertThat(result).isNull();
+    }
 }
