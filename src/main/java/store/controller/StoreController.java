b47b0ccd9d80728c4dedc582ad331d88a4fe9832
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-11 10:54:58 +0900

    feat: 예외 시 반복 시작

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -3,7 +3,6 @@ package store.controller;
 import store.domain.Receipt;
 import store.service.StoreService;
 
-import store.service.UserInteractionCallback;
 import store.util.exception.RestartException;
 import store.view.InputView;
 import store.view.OutputView;
@@ -24,27 +23,59 @@ public class StoreController implements UserInteractionCallback {
     }
 
     public void run() {
-        try {
-            storeOpeningMessage();
-            customerPurchase();
-            membershipMessage();
-        } catch (RestartException re) {
-            run();
+        while (true) {
+            try {
+                storeOpeningMessage();
+                customerPurchase();
+                otherPurchaseMessage();
+            } catch (RestartException re) {
+                // 재시작 예외를 캐치하여 루프를 재실행
+                continue;
+            }
+            // 재시작 예외가 발생하지 않는 경우 루프 종료
+            break;
         }
+        System.out.println("프로그램이 종료되었습니다.");
     }
+
     private void storeOpeningMessage() {
         String openiningMent = storeService.generateOpeningMessage();
         outputView.printProducts(openiningMent);
     }
 
     private void customerPurchase() {
-        String answer = inputView.readItem();
-        Receipt receipt = storeService.processPayment(answer);
-        outputView.printReceipt(receipt.generateReceiptString());
+        while (true) {
+            try {
+                String answer = inputView.readItem();
+                Receipt receipt = storeService.processPayment(answer);
+                membershipMessage(receipt);
+                outputView.printReceipt(receipt.generateReceiptString());
+                break;  // 성공적으로 실행되면 루프를 종료
+            } catch (IllegalArgumentException e) {
+                outputView.printError(e.getMessage());  // 예외 메시지 출력
+            }
+        }
     }
 
 
-    private void membershipMessage() {
+    private void membershipMessage(Receipt receipt) {
+        String answer = inputView.membershipMessage();
+        if (answer.equals("Y")) {
+            storeService.calculateMembership(receipt);
+        }
+        if (answer.equals("N")) {
+            storeService.calculateRegular(receipt);
+        }
+    }
+
+    private void otherPurchaseMessage() {
+        String answer = inputView.otherPurchaseMessage();
+        if (answer.equals("Y")) {
+            throw new RestartException();
+        }
+        if (answer.equals("N")) {
+
+        }
     }
 
 
