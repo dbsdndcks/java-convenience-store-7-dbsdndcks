567fff65f5470261753f2e6f05e71e95f38fbfd4
DEV_WOONG <ywcsuwon@gmail.com>
2024-11-11 22:24:32 +0900

    feat: 입력 검증 기능 추가

--- src/main/java/store/util/validator/InputValidator.java
+++ src/main/java/store/util/validator/InputValidator.java
@@ -1,4 +1,32 @@
 package store.util.validator;
 
+import store.util.Message.ErrorMessage;
+
+import java.util.regex.Pattern;
+
 public class InputValidator {
+    private static final Pattern INPUT_PATTERN = Pattern.compile("^\\[(.+)-(\\d+)](,\\[(.+)-(\\d+)])*$");
+
+    public static void validateInputQuestion(String input) {
+        validateInputIsEmpty(input);
+        validateMembershipInput(input);
+    }
+
+    private static void validateInputIsEmpty(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT.getMessage());
+        }
+    }
+
+    private static void validateMembershipInput(String input) {
+        if (!input.equals("Y") && !input.equals("N")) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT.getMessage());
+        }
+    }
+
+    public static void validateInputProduct(String input) {
+        if (!INPUT_PATTERN.matcher(input).matches()) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_FORMAT.getMessage());
+        }
+    }
 }
